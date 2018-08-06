5d1fc5959b1a25fae37c9957658d88e61d6b139d
danielCull <daniel.cullhaj@stud.unifi.it>
2018-08-06 21:23:01 +0200

    Adding operator overloading and trying the transpose function.

--- MatrixTemplate.h
+++ MatrixTemplate.h
@@ -37,25 +37,62 @@ public:
     void printMatrix(MatrixTemplate& matrixTemplate) {
         for(int i = 0; i<_rows; i++) {
             for (int j = 0; j < _columns; j++)
-                std::cout << "[" << _buffer[j + i * _columns] << "]";
+                std::cout << "  [" << _buffer[j + i * _columns] << "]";
             std::cout << std::endl;
         }
-
+    }
+    void printMatrix(){
+        for(int i = 0; i<_rows; i++) {
+            for (int j = 0; j < _columns; j++)
+                std::cout << "  [" << _buffer[j + i * _columns] << "]";
+            std::cout << std::endl;
+        }
+    }
+    T elementPosition(int rowPos, int colPos) const{
+        T element = _buffer[rowPos + colPos * _columns];
+        return element;
     }
 
 
 
-
-
-
     /*Operator overloading*/
-    bool operator!=(const MatrixTemplate& mt);
-    bool operator==(const MatrixTemplate& mt);
+    MatrixTemplate& operator=(const MatrixTemplate<T>& mt) {
+        if(this != &mt){
+            delete[] _buffer;
+            copier(mt);
+        }return *this;
+    }
+    bool operator==(const MatrixTemplate<T>& mt) const{
+        if(_rows == mt._columns){
+            for(int i = 0; i < _rows; i++){
+                for(int j = 0; j < _columns; j++)
+                    if(mt.elementPosition(i,j) != _buffer[j + i * _columns])
+                        return true;
+            return false;
+            }
+        }
+        return true;
+    }
+    MatrixTemplate operator+(const MatrixTemplate<T>& mt){
+        if(_rows == mt._columns){
+            MatrixTemplate<T> sumMatrix(_rows, _columns);
+            for(int i = 0; i<_rows*_columns; i++){
+                mt._buffer[i] = _buffer[i] + mt._buffer[i];
+            }
+            return sumMatrix;
+        }
+        std::cout << "ERROR SUM MATRIX" << std::endl;
+    }
 
     /*Destructor*/
     ~MatrixTemplate(){
         delete[] _buffer;
     }
+
+    /*Getters and setters*/
+    int getRows() const { return _rows; }
+    int getColumns() const { return _columns; }
+
 private:
     int _rows, _columns;
     T* _buffer;
--- main.cpp
+++ main.cpp
@@ -5,12 +5,20 @@ int main() {
     MatrixTemplate<int> matrixTemplate(3,3);
     std::cout << "ORIGINAL MATRIX " << std::endl;
     matrixTemplate.printMatrix(matrixTemplate);
-
     std::cout << std::endl;
 
-    MatrixTemplate<int> trasposeMatrix = matrixTemplate.getTranspose();
-    std::cout << "TRANSPOSE MATRIX " << std::endl;
-    trasposeMatrix.printMatrix(trasposeMatrix);
+    MatrixTemplate<int> testMatrixTemplate = matrixTemplate;
+    std::cout << "TEST MATRIX TEMPLATE" << std::endl;
+    testMatrixTemplate.printMatrix(testMatrixTemplate);
+    std::cout << std::endl;
+
+    MatrixTemplate<int> transposeMatrix = matrixTemplate.getTranspose();
+    std::cout << "TRANSPOSE MATRIX" << std::endl;
+    transposeMatrix.printMatrix();
+    std::cout << std::endl;
+
+
+
 
 
     return 0;
