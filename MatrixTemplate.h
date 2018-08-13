efe51ee4ed67719d74df83ac15da1f64b3ae9cbf
danielCull <daniel.cullhaj@stud.unifi.it>
2018-08-13 12:59:50 +0200

    Create MatrixException class and modifying the code inserting exceptions.

--- MatrixTemplate.h
+++ MatrixTemplate.h
@@ -6,21 +6,22 @@
 #define MATRIXABSFACTORY_MATRIXTEMPLATE_H
 
 #include "headers.h"
+#include "MatrixException.h"
 
 template <class T>
 class MatrixTemplate {
 public:
     /*Constructors and Destructor.*/
-    MatrixTemplate(int r, int c) ; /*tested*/
+    MatrixTemplate(int r, int c) throw(MatrixException); /*tested*/
     MatrixTemplate(const MatrixTemplate& mt);/*tested*/
     ~MatrixTemplate(); /*tested*/
 
     /*Matrix Operations*/
     virtual MatrixTemplate getTranspose() const;/*tested*/
-    virtual MatrixTemplate<T> selectRow(int rowNum);/*tested*/
-    virtual MatrixTemplate<T> selectColumn(int colNum);/*tested*/
+    virtual MatrixTemplate<T> selectRow(int rowNum) const throw(MatrixException);/*tested*/
+    virtual MatrixTemplate<T> selectColumn(int colNum) const throw(MatrixException);/*tested*/
     virtual T elementPosition(int rowPos, int colPos) const; /*tested*/
-    virtual void modifyElement(int rowNum, int colNum, const T& newValue); /*tested*/
+    virtual void modifyElement(int rowNum, int colNum, const T& newValue) throw(MatrixException); /*tested*/
     virtual void manualInsertValues(MatrixTemplate& newMatrixTemplate); /*tested*/
     virtual void matrixOfZeros(); /*tested*/
     virtual void randomMatrix(); /*tested*/
@@ -31,17 +32,24 @@ public:
     virtual void printMatrix(); /*tested*/
 
 
+
     /*Operator overloading*/
     virtual MatrixTemplate<T>& operator=(const MatrixTemplate<T>& mt);/*tested*/
-    virtual MatrixTemplate<T> operator+(const MatrixTemplate<T>& mt);/*tested*/
-    virtual MatrixTemplate<T> operator*(const MatrixTemplate<T>& mt) const; /*tested*/
+
+
+    virtual MatrixTemplate<T> operator+(const MatrixTemplate<T>& mt) const throw(MatrixException);/*tested*/
+    virtual MatrixTemplate<T> operator*(const MatrixTemplate<T>& mt) const throw(MatrixException); /*tested*/
+    virtual MatrixTemplate<T> operator-(const MatrixTemplate<T>& mt) const throw(MatrixException);/*tested*/
+    virtual MatrixTemplate<T> operator+=(const MatrixTemplate<T>& mt) throw(MatrixException);/*tested*/
+
     virtual MatrixTemplate<T> operator*(const T& num);/*tested*/
-    virtual MatrixTemplate<T> operator/(const MatrixTemplate<T>& mt);/*tested*/
     virtual MatrixTemplate<T> operator/(const T& num); /*tested*/
-    virtual MatrixTemplate<T> operator-(const MatrixTemplate<T>& mt);/*tested*/
-    virtual MatrixTemplate<T> operator+=(const MatrixTemplate<T>& mt);/*tested*/
+
+
+
     virtual bool operator==(const MatrixTemplate<T>& mt) const;/*tested*/
     virtual bool operator!=(const MatrixTemplate<T>& mt) const;/*tested*/
+
 
     /*Getters and setters*/
     virtual int getRows() const ; /*tested*/
@@ -74,12 +82,9 @@ protected:
 
 /*Constructors and Destructors.*/
 template <class T>
-MatrixTemplate<T>::MatrixTemplate(int r, int c) : _rows(r), _columns(c) {
-    if (r < 1)
-        _rows = 1;
-    if (c < 1)
-        _columns = 1;
-
+MatrixTemplate<T>::MatrixTemplate(int r, int c) throw(MatrixException) : _rows(r), _columns(c) {
+    if(this->_rows <= 0 || this->_columns <= 0)
+        throw MatrixException("Number of rows and columns must be positive.");
     _buffer = new T[_rows * _columns];
     srand((unsigned)time(0));
     for(int i = 0; i <_rows*_columns; i++)
@@ -146,31 +151,32 @@ T MatrixTemplate<T>::elementPosition(int rowPos, int colPos) const {
 }
 
 template <class T>
-MatrixTemplate<T> MatrixTemplate<T>::selectRow(int rowNum) {
-    if(rowNum>=1){
-        MatrixTemplate<T> rowMatrix(1, _columns);
-        for(int i = 0; i<_columns; i++)
-            rowMatrix._buffer[i] = _buffer[i+_columns*(rowNum-1)];
-        return rowMatrix;
-    }
-    std::cout << "ERROR ROW MATRIX" << std::endl;
+MatrixTemplate<T> MatrixTemplate<T>::selectRow(int rowNum) const throw(MatrixException) {
+    if(this->_rows <= 0 || this->_rows > rowNum)
+        throw MatrixException("Invalid row selection.");
+
+    MatrixTemplate<T> rowMatrix(1, _columns);
+    for(int i = 0; i<_columns; i++)
+        rowMatrix._buffer[i] = _buffer[i+_columns*(rowNum-1)];
+    return rowMatrix;
 }
 
 template <class T>
-MatrixTemplate<T> MatrixTemplate<T>::selectColumn(int colNum) {
-    if(colNum >= 1){
-        MatrixTemplate<T> columnMatrix(_rows, 1);
-        for(int j = 0; j<_rows; j++)
-            columnMatrix._buffer[j] = _buffer[j+_rows*(colNum-1)];
-        return columnMatrix;
-    }
-    std::cout << "ERROR COLUMNS MATRIX" << std::endl;
+MatrixTemplate<T> MatrixTemplate<T>::selectColumn(int colNum) const throw(MatrixException) {
+    if(this->_columns <= 0 || this->_columns > colNum)
+        throw MatrixException("Invalid columns selection.");
+
+    MatrixTemplate<T> columnMatrix(_rows, 1);
+    for(int j = 0; j<_rows; j++)
+        columnMatrix._buffer[j] = _buffer[j+_rows*(colNum-1)];
+    return columnMatrix;
 }
 
 template <class T>
-void MatrixTemplate<T>::modifyElement(int rowNum, int colNum, const T& newValue) {
-    if(_columns > 0 || _rows > 0)
-        _buffer[(rowNum-1)* _columns + (colNum-1)] = newValue;
+void MatrixTemplate<T>::modifyElement(int rowNum, int colNum, const T& newValue) throw(MatrixException) {
+    if(this->_rows <= 0 || this->_rows > rowNum || this->_columns <= 0 || this->_columns > colNum)
+        throw MatrixException("Invalid number index.");
+    _buffer[(rowNum-1)* _columns + (colNum-1)] = newValue;
 }
 
 template<class T>
@@ -243,43 +249,29 @@ bool MatrixTemplate<T>::operator!=(const MatrixTemplate<T>& mt) const {
 }
 
 template <class T>
-MatrixTemplate<T> MatrixTemplate<T>::operator+(const MatrixTemplate<T>& mt){
-    try {
-        if (_rows == mt._rows && _columns == mt._columns) {
-            MatrixTemplate<T> sumMatrix(_rows, _columns);
-            for (int i = 0; i < _rows * _columns; i++) {
-                sumMatrix._buffer[i] = _buffer[i] + mt._buffer[i];
-            }
-            return sumMatrix;
-        }else{
-            throw std::out_of_range("ERROR -2: Rows and columns of two matrix are not equal.");
-        }
-    }catch (std::out_of_range& e){
-        e.what();
+MatrixTemplate<T> MatrixTemplate<T>::operator+(const MatrixTemplate<T>& mt) const throw(MatrixException){
+    if(this->_rows != mt._rows || this->_columns != mt._columns)
+        throw MatrixException("Not compatible matrix for addition operation.");
+    MatrixTemplate<T> sumMatrix(_rows, _columns);
+    for (int i = 0; i < _rows * _columns; i++) {
+        sumMatrix._buffer[i] = _buffer[i] + mt._buffer[i];
     }
-
+    return sumMatrix;
 }
 
 template <class T>
-MatrixTemplate<T> MatrixTemplate<T>::operator*(const MatrixTemplate<T> &mt) const {
-    try {
-        if (_rows == mt._columns) {
-            MatrixTemplate<T> prodMatrix(_rows, mt._columns);
-            for (int i = 0; i < _rows; i++)
-                for (int j = 0; j < mt._columns; j++) {
-                    prodMatrix._buffer[i * mt._columns + j] = 0;
-                    for (int h = 0; h < _columns; h++)
-                        prodMatrix._buffer[i * mt._columns + j] +=
-                                _buffer[i * _columns + h] * mt._buffer[h * mt._columns + j];
-                }
-            return prodMatrix;
-        }else{
-            throw std::out_of_range("ERROR -1: First matrix row number not equal with second's matrix columns number.");
+MatrixTemplate<T> MatrixTemplate<T>::operator*(const MatrixTemplate<T> &mt) const throw(MatrixException){
+    if(this->_columns != mt._rows)
+        throw MatrixException("Now compatible columns and rows for moltiplication.");
+    MatrixTemplate<T> prodMatrix(_rows, mt._columns);
+    for (int i = 0; i < _rows; i++)
+        for (int j = 0; j < mt._columns; j++) {
+            prodMatrix._buffer[i * mt._columns + j] = 0;
+            for (int h = 0; h < _columns; h++)
+                prodMatrix._buffer[i * mt._columns + j] +=
+                        _buffer[i * _columns + h] * mt._buffer[h * mt._columns + j];
         }
-    }catch (std::out_of_range& e){
-        e.what();
-    }
-
+    return prodMatrix;
 }
 
 template <class T>
@@ -291,19 +283,6 @@ MatrixTemplate<T> MatrixTemplate<T>::operator*(const T& num){
 }
 
 template <class T>
-MatrixTemplate<T> MatrixTemplate<T>::operator/(const MatrixTemplate<T>& mt){
-    if(_rows == mt._columns && _columns == mt._rows){
-        MatrixTemplate<T> divMatrix(_rows, _columns);
-        for(int i =0; i<_rows*_columns; i++){
-            divMatrix._buffer[i] = _buffer[i] / mt._buffer[i];
-        }
-        return divMatrix;
-    }
-    //return mt;
-    std::cout << "ERROR DIV MATRIX" << std::endl;
-}
-
-template <class T>
 MatrixTemplate<T> MatrixTemplate<T>::operator/(const T &num) {
     MatrixTemplate<T> divNum(_rows,_columns);
     for(int i = 0; i <_rows*_columns; i++)
@@ -312,31 +291,22 @@ MatrixTemplate<T> MatrixTemplate<T>::operator/(const T &num) {
 }
 
 template <class T>
-MatrixTemplate<T> MatrixTemplate<T>::operator-(const MatrixTemplate<T>& mt){
-    try {
-        if (_rows == mt._columns && _columns == mt._rows) {
-            MatrixTemplate<T> subMatrix(_rows, _columns);
-            for (int i = 0; i < _rows * _columns; i++) {
-                subMatrix._buffer[i] = _buffer[i] - mt._buffer[i];
-            }
-            return subMatrix;
-        }else{
-            throw std::out_of_range("ERROR -3: Rows and columns of two matrix are not equal.");
-        }
-    }catch(std::out_of_range& e){
-        e.what();
+MatrixTemplate<T> MatrixTemplate<T>::operator-(const MatrixTemplate<T>& mt) const throw(MatrixException){
+    if(this->_rows != mt._rows || this->_columns != mt._columns)
+        throw MatrixException("Not compatible matrix for subtraction operation.");
+    MatrixTemplate<T> subMatrix(_rows, _columns);
+    for (int i = 0; i < _rows * _columns; i++) {
+        subMatrix._buffer[i] = _buffer[i] - mt._buffer[i];
     }
+    return subMatrix;
 }
 
 template <class T>
-MatrixTemplate<T> MatrixTemplate<T>::operator+=(const MatrixTemplate<T>& mt){
-    if(_rows == mt._columns && _columns == mt._rows){
-        for(int i =0; i<_rows*_columns; i++){
-            _buffer[i] = _buffer[i] + mt._buffer[i];
-        }
-        return *this;
-    }else{
-        std::cout << "ERROR += " << std::endl;
+MatrixTemplate<T> MatrixTemplate<T>::operator+=(const MatrixTemplate<T>& mt) throw(MatrixException){
+    if(this->_rows != mt._rows || this->_columns != mt._columns)
+        throw MatrixException("Matrix are not compatible.");
+    for(int i =0; i<_rows*_columns; i++){
+        _buffer[i] = _buffer[i] + mt._buffer[i];
     }
 }
 
--- Utils.h
+++ Utils.h
@@ -10,14 +10,44 @@
 
 class Utils {
 public:
-    void matrixInfo();
-    void fillMatrix();
-    void optionsScreen();
-    void line();
-    void clearScreen();
-    void readMatrix(int& rows, int& columns, std::string mName);
-    void readMatrix(int& rows, int& columns);
+    void matrixInfo(){
+        system("clear");
+        std::cout << "\tPOSSIBLE MATRIX SELECTION" << std::endl;
+        std::cout << "\t#########################" << std::endl;
+        std::cout << "1) Row Matrix\n2) Columns Matrix\n3) Square Matrix\n4) Rectangular Matrix" << std::endl;
+        line();
+    }
+    void fillMatrix(){
+        std::cout << "\n\tFILL THE MATRIX" << std::endl;
+        std::cout << "\t################" << std::endl;
+        std::cout << "0) Zero values Matrix\n1) Random fill matrix.\n2) Manual fill matrix.\n3) Order Incremented Matrix." << std::endl;
+    }
+    void optionsScreen() {
+        std::cout <<"\n\tAVAILABLE MATRIX OPERATIONS" << std::endl;
+        std::cout << "\t###########################" << std::endl;
+        std::cout << "0) Create Another Matrix." << std::endl;
+        std::cout << "1) Show available matrix.\n2) Multiply Matrix.\n3) Multiply Matrix by Number." << std::endl;
+        std::cout << "4) Edit Value Matrix.\n5) Divide Matrix by Number.\n6) Adding Matrix.\n7) Subtracting Matrix." << std::endl;
+        std::cout << "8) Select Row of the Matrix.\n9) Select Columna of the Matrix.\n10) UNDER CONSTRUCTION" << std::endl;
+        std::cout << "99) Exit from the program." << std::endl;
+    }
+    void line(){
+        std::cout << std::endl;
+    }
+    void clearScreen(){
+        line();
+        std::cout << "\nPress any key to continue..." << std::endl;
+        std::cin.ignore().get();
+        system("clear");
+    }
+    void readMatrix(int& rows, int& columns, std::string mName){
+        std::cout <<"\nEnter " << mName << " matrix rows: "; std::cin >> rows;
+        std::cout << "Enter " <<  mName << " matrix columns: "; std::cin >> columns;
+    }
+    void readMatrix(int& rows, int& columns) {
+        std::cout <<"\nEnter matrix rows: "; std::cin >> rows;
+        std::cout << "Enter matrix columns: "; std::cin >> columns;
+    }
 };
-
 
 #endif //MATRIXABSFACTORY_UTILS_H
