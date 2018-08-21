d3b8116b43e55ce7ba9e260dadf9ad82bc4388bc
danielCull <daniel.cullhaj@stud.unifi.it>
2018-08-21 18:48:15 +0200

    Removing square and retaungular matrix from factory.

--- MatrixFactoryType.h
+++ MatrixFactoryType.h
@@ -6,9 +6,6 @@
 #define MATRIXABSFACTORY_MATRIXFACTORYTYPE_H
 
 #include "MatrixFactory.h"
-#include "SquareMatrix.h"
-#include "RectangularMatrix.h"
-
 
 template <class T>
 class MatrixFactoryType : public MatrixFactory<T>{
@@ -21,19 +18,19 @@ MatrixTemplate<T>* MatrixFactoryType<T>::createMatrix(int r, int c) throw(std::o
     MatrixTemplate<T>* matrixTemplate;
     std::cout << std::endl;
     if(r == 1 && c > 0) {
-        matrixTemplate = new RectangularMatrix<T>(1, c);
+        matrixTemplate = new MatrixTemplate<T>(1, c);
         std::cout << "You have selected a row (" <<r <<  "x" << c <<  ") matrix." << std::endl;
         matrixTemplate->setMatrixName("ROW");
     }else if(r > 0 && c == 1) {
-        matrixTemplate = new RectangularMatrix<T>(r, 1);
+        matrixTemplate = new MatrixTemplate<T>(r, 1);
         std::cout << "You have selected a column (" <<r <<  "x" << c <<  ") matrix." << std::endl;
         matrixTemplate->setMatrixName("COLUMN");
     }else if(r == c) {
-        matrixTemplate = new SquareMatrix<T>(r, c);
+        matrixTemplate = new MatrixTemplate<T>(r, c);
         std::cout << "You have selected a square (" <<r <<  "x" << c <<  ") matrix." << std::endl;
         matrixTemplate->setMatrixName("SQUARE");
     }else if(r > 0 && c > 0 && r !=c ) {
-        matrixTemplate = new RectangularMatrix<T>(r, c);
+        matrixTemplate = new MatrixTemplate<T>(r, c);
         std::cout << "You have selected a rectangular (" <<r <<  "x" << c <<  ") matrix." << std::endl;
         matrixTemplate->setMatrixName("RECTANGULAR");
     }else{
